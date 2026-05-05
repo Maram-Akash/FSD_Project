9a77dda36876312c9d3a6694ded23e349f6e814f
Maram-Akash <vtu24837@veltech.edu.in>
2026-05-06 00:40:08 +0530

    Delete FSAD directory

--- FSAD/FSAD/src/main/java/com/ticketbooking/repository/TheatreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ticketbooking.repository;
-
-import com.ticketbooking.entity.Theatre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheatreRepository extends JpaRepository<Theatre, Long> {
-}
